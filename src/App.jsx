17110ea4cf71d1398e846fa1511df131bb39fa33
Sarthak <sarthaksharma593@gmail.com>
2022-09-27 00:08:32 +0530

    created board and square components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Board from "./Components/Board";
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>TIC TAC TOE!</h1>
-    </>
+      <Board />
+    </div>
   );
 };
 export default App;
